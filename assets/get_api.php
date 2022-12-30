630c9d39c93e35f457f58c93711250333ef1dfcc
Shuja ur Rahman <63500837+shujaurrahman@users.noreply.github.com>
2022-12-30 06:03:39 +0530

    Update get_api.php

--- assets/get_api.php
+++ assets/get_api.php
@@ -4,7 +4,7 @@
  $headr = array();
  $headr[] = 'Accept: application/json';
  $headr[] = 'Content-type: application/json';
- $headr[] = 'Authorization: Bearer AQCwZlkGfHYkKGRWXVB7i59LqQX0hzzmM725w-Vpwg9BFzDYJJrgfWKIwh9baiRj9LhoiVSFWdnrTii3EaYN7_3M_XJ0YnnHvHlBvNoEmHyjZHPnRQa1OEMdDbC8NbPGDdDH8-O_Hc_a5780weT8lTkNNwUVmEyHyJ5MPjnlOj6E';
+ $headr[] = 'Authorization: Bearer <token>';
  curl_setopt($curl, CURLOPT_URL,"https://api.spotify.com/v1/me/player/currently-playing");
  curl_setopt($curl, CURLOPT_HTTPHEADER,$headr);
  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
